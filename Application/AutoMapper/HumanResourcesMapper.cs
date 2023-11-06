a6849255766767f154bb9a2d24313b41a216fbe6
omerfdev <omerfdev@gmail.com>
2023-11-06 23:26:35 +0300

    Commit

--- Application/AutoMapper/HumanResourcesMapper.cs
+++ Application/AutoMapper/HumanResourcesMapper.cs
@@ -1,4 +1,7 @@
 ﻿using Application.Models.DTOs.AppUserDTO;
+using Application.Models.DTOs.CompanyDTO;
+using Application.Models.DTOs.DepartmentDTO;
+using Application.Models.DTOs.SpendingDTO;
 using Application.Models.DTOs.SpendingTypeDTO;
 using AutoMapper;
 using Domain.Entities.Concrete;
@@ -18,6 +21,12 @@ namespace Application.AutoMapper
             CreateMap<AppUser, UpdateEmployeeDTO>().ReverseMap();
             CreateMap<SpendingType, AddSpendingTypeDTO>().ReverseMap();
             CreateMap<SpendingType, UpdateSpendingTypeDTO>().ReverseMap();
+            CreateMap<Spending, AddSpendingDTO>().ReverseMap();
+            CreateMap<Spending, UpdateSpendingDTO>().ReverseMap();
+            CreateMap<Department, AddDepartmentDTO>().ReverseMap();
+            CreateMap<Department, UpdateDepartmentDTO>().ReverseMap();
+            CreateMap<Company, AddCompanyDTO>().ReverseMap();
+            CreateMap<Company, UpdateCompanyDTO>().ReverseMap();
         }
     }
 }
